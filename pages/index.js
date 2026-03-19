1101cec98a76ad1b93fbf23a2d252fefecc0a403
Maurício Correa <correamauricio.design@gmail.com>
2026-03-19 03:14:17 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Brenda, você é o meu <i>localhost</i>, o único lugar onde eu me sinto em casa. Te amo!</h1>
+  return (
+    <h1>
+      Brenda, você é o meu <i>localhost</i>, o único lugar onde eu me sinto em
+      casa. Te amo!
+    </h1>
+  );
 }
 
 export default Home;
