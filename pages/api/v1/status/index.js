139695bb07b4beb8075e238b2e331d8e8a61269a
Maurício Correa <correamauricio.design@gmail.com>
2026-05-03 18:20:20 +0000

    finaliza endpoint `/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,8 +2,34 @@ import database from "infra/database.js";
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
+
+  const databaseVersion = await database.query("SHOW server_version;");
+  const databaseVersionValue = parseFloat(
+    databaseVersion.rows[0].server_version,
+  );
+
+  const databaseMaxConnections = await database.query("SHOW max_connections;");
+  const databaseMaxConnectionsValue = parseInt(
+    databaseMaxConnections.rows[0].max_connections,
+  );
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnections.rows[0].count;
+
   response.status(200).json({
     updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: databaseMaxConnectionsValue,
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -7,4 +7,17 @@ test("GET to /api/v1/status should return 200", async () => {
 
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toBe(parsedUpdatedAt);
+
+  const databaseVersion = responseBody.dependencies.database.version;
+  expect(databaseVersion).toBeGreaterThanOrEqual(16);
+
+  const databaseMaxConnections =
+    responseBody.dependencies.database.max_connections;
+  expect(databaseMaxConnections).toBeGreaterThan(0);
+  expect(databaseMaxConnections).toEqual(expect.any(Number));
+  expect(Number.isInteger(databaseMaxConnections)).toBe(true);
+
+  const databaseOpenedConnections =
+    responseBody.dependencies.database.opened_connections;
+  expect(databaseOpenedConnections).toBe(1);
 });
